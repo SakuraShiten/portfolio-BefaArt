6ecdd0f7e9561280b2be615542a8cae8eb0cef27
Sakura <kopolartec.artem@gmail.com>
2021-11-06 19:40:17 +0500

    project

---
+++ js/main.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    main();
+})
+function main() {
+    initBtnPortfolio();
+};
+function initBtnPortfolio() {
+    let arrayBtnInfoIcon = document.querySelectorAll(".icon-info");
+    let timeout = true;
+    for (let obj of Array.from(arrayBtnInfoIcon)) {
+        let onClickInfo = function () {
+            if (!timeout) return;
+            timeout = false;
+            setTimeout(() => {
+                timeout = true;
+            }, 500);
+            let textAbout = obj.parentNode.querySelector("h2");
+            let textAboutHeader = obj.parentNode.querySelector("h3");
+            let textAboutMain = obj.parentNode.querySelector("p");
+            let iconExit = obj.parentNode.querySelector(".icon-info-exit");
+            let icon = obj.parentNode.querySelector(".icon-info");
+            fadeOut(300, icon, textAbout)
+            fadeIn(300, textAboutHeader, textAboutMain, iconExit)
+        }
+        obj.addEventListener('click', onClickInfo);
+        let objExit = obj.parentNode.querySelector(".icon-info-exit");
+        let exitInfo = function () {
+            if (!timeout) return;
+            timeout = false;
+            setTimeout(() => {
+                timeout = true;
+            }, 500);
+            let textAbout = obj.parentNode.querySelector("h2");
+            let textAboutHeader = obj.parentNode.querySelector("h3");
+            let textAboutMain = obj.parentNode.querySelector("p");
+            let iconExit = obj.parentNode.querySelector(".icon-info-exit");
+            let icon = obj.parentNode.querySelector(".icon-info");
+            fadeIn(300, icon, textAbout)
+            fadeOut(300, textAboutHeader, textAboutMain, iconExit)
+
+        }
+        objExit.addEventListener('click', exitInfo);
+    }
+}
+function fadeOut(time, ...elem) {
+    for (let obg of elem) {
+        obg.style.opacity = 0;
+        setTimeout(() => {
+            obg.style.display = "none";
+        }, time);
+    }
+}
+function fadeIn(time, ...elem) {
+    for (let obj of elem) {
+        obj.style.opacity = 0;
+        obj.style.display = "block";
+        op = 0;
+        time = time / 10
+        let timer = setInterval(() => {
+            op += 0.1;
+            obj.style.opacity = op;
+            if (op >= 1) {
+                clearInterval(timer);
+                obj.style.opacity = 1;
+            };
+        }, time);
+    }
+}
